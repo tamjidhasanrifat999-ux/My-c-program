e687b0379bf1f069df96c541d1b1756748dc828b
Tamjid Hasan Rifat <tamjidhasanrifat999@gmail.com>
2026-06-29 07:30:37 +0600

    Add files via upload
    pratice c

---
+++ Module 05/Welcome_with_conditions.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+   int A,B;
+   scanf("%d %d",&A,&B) ;
+   if (A>=B)
+   {
+    printf("YES\n");
+   }
+   else
+   {
+    printf("NO\n");
+   }
+    return 0;
+}
---
+++ Module 05/maximanorminimam.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+   int a, b, c;
+   scanf("%d %d %d",&a ,&b, &c);
+
+   //Minimam
+   if(a < b && a < c)
+   {
+    printf("%d ", a);
+   }
+   else if(b < a && b < c)
+   {
+    printf("%d ", b);
+   }
+   else if(c < a && c < b)
+   {
+    printf("%d ", c);
+   }
+
+   // Maximam
+       if (a > b && a < c)
+   {
+    printf("%d ", a);
+   }
+   else if(b > a && b > c)
+   {
+    printf("%d ", b);
+   }
+   else if (c > a && c > b)
+   {
+    printf("%d ", c);
+   }
+    return 0;
+}
