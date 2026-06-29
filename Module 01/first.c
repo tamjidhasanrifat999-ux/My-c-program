b59cfa373e701424d308eeb25c268e0897297687
Tamjid Hasan Rifat <tamjidhasanrifat999@gmail.com>
2026-06-29 07:28:18 +0600

    Add files via upload

---
+++ Module 01/first.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    printf("My name is Tamjid\n");
+
+    printf("My name is Tamjid\nBut surname is Rifat\n");
+    
+    printf("Hello\t World\n");
+
+int num2 = 210;
+printf("%d\n",num2);
+
+float f = 33.3;
+
+printf("%.2f\n",f);
+
+char c = '#';
+printf("%c",c);
+    return 0;
+}
+
+
+
---
+++ Module 01/input.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+   long long int a= 10000000000000;
+    printf("%lld",a);
+    return 0;
+}
