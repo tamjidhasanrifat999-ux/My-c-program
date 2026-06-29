0435b31730c225396db8b89f39c4e55c3126c8cd
Tamjid Hasan Rifat <tamjidhasanrifat999@gmail.com>
2026-06-29 07:38:46 +0600

    Add files via upload

---
+++ Module 14/No_return_+prameters.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void sum(int a,int b)
+{
+    int ans = a+b;
+    
+    printf("%d",ans);
+
+}
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    sum (a,b);
+   
+    return 0;
+}
